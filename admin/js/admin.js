e4861a1ed0202816575dacfe2686f0458471a066
mlehelsz <mlehelsz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-13 15:31:37 +0000

    = 2.1 = * New feature - Google fonts preview when edit the style * New feature - List all styles thumbnail preview for predefined templates
    git-svn-id: https://plugins.svn.wordpress.org/contact-form-7-style/tags/2.1@1089157 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/js/admin.js
@@ -0,0 +1,27 @@
+/*
+jQuery functions for the Admin area
+*/
+function changeFont( value ) {
+	$(".google-fontos").remove();
+	if( "none" != value ) {
+		$("head").append('<link class="google-fontos" rel="stylesheet" href="http://fonts.googleapis.com/css?family='+value+':100,200,300,400,500,600,700,800,900&subset=latin,latin-ext,cyrillic,cyrillic-ext,greek-ext,greek,vietnamese" />');
+		$(".cf7-style.preview-zone p").css("font-family", "'"+value+"', sans-serif" );
+	}
+}
+jQuery(document).ready( function( $ ) {
+
+	var selectVar = $('select[name=cf7_style_font_selector]');
+
+	$('.cf7-style-color-field').wpColorPicker();
+
+	$("#select_all").on("click", function(){
+		$(".cf7style_body_select_all input").prop('checked', ( $(this).is(":checked") ) ? true : false );
+	});
+
+	changeFont( selectVar.val() );
+	selectVar.on( "change", function(){
+		changeFont( $( this ).val() );
+	});
+
+});
+
